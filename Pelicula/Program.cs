f42ad1c5320aeeeeb9bf97543d8d60e8b55a6b67
Eduardo Galeana <124205317+L4l0G@users.noreply.github.com>
2023-03-22 01:46:47 +0000

    [MétodosPelicula]

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -7,38 +7,63 @@ namespace Pelicula
     public class Pelicula
     {
         //Campo
-        public string Titulo;
-        public Int16 Año;
-        public string País;
-        public string Director;
+        private string Titulo {get; set;}
+        private Int16 Año;
+        private string País;
+        private string Director;
         private List<Actor> actores = new List<Actor>();
         //Constructores
-     public Pelicula ()
-     {
-
-     }
-
+   
      public Pelicula(string Titulo, Int16 Año)
      {
         this.Titulo = Titulo;
         this.Año = Año;
      }
+
+     public Pelicula()
+     {
+
+     }
         //Métodos
-     
+        public string SetTitulo(string Titulo)
+        {
+            this.Titulo = Titulo;
+            return Titulo;
+        }
+
+        public Int16 SetAño(Int16 Año)
+        {
+            this.Año = Año;
+            return Año;
+        }
+
+        public string GetTitulo()
+        {
+            return Titulo;
+        }
+
+        public Int16 GetAño()
+        {
+            return Año;
+        }
+
         public void Imprime()
         {
             Console.WriteLine($"{Titulo}({Año})");
         }
 
-
     }
 
     public class Actor
     {
+        private string Nombre;
+        private Int16 Año;
+        private string País;
+        private Int16 Edad;
+
         //Propiedades
 
         //Constructores
-
 
         //Métodos 
         public void Imprime()
@@ -54,13 +79,13 @@ namespace Pelicula
     {
         static void Main(string[] args)
         {
-           Pelicula p1= new Pelicula();
-           Pelicula p2 = new Pelicula();
            Pelicula p1 = new Pelicula();
+           Pelicula p2 = new Pelicula();
            p1.SetTitulo("Parasite");
            p1.SetAño(2019);
            Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
-
+           p2.SetTitulo("Everything Everywhere All At Once");
+           p2.SetAño(2022);
         }
     }
 } 
