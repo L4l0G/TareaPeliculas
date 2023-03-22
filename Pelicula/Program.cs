13edb4d9d19065a09d1c43ec96aae1cd7fcc65e5
Eduardo Galeana <124205317+L4l0G@users.noreply.github.com>
2023-03-22 03:15:16 +0000

    [Cambio de método AñadirActor en Main]

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -94,12 +94,12 @@ namespace Pelicula
            Pelicula p2 = new Pelicula();
            p1.SetTitulo("Parasite");
            p1.SetAño(2019);
-           p1.AñadirActor(new Actor("Song Kang-ho", 1967));
+           p1.AgregaActor(new Actor("Song Kang-ho", 1967));
            p1.ImprimeActores();
            Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
            p2.SetTitulo("Everything Everywhere All At Once");
            p2.SetAño(2022);
-           p2.AñadirActor(new Actor ("Michelle Yeoh", 1962));
+           p2.AgregaActor(new Actor ("Michelle Yeoh", 1962));
            p2.ImprimeActores();
            Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño());
         }
