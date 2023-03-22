001ddf84ea244e076fa9e5f3046772af6a1618f6
Eduardo Galeana <124205317+L4l0G@users.noreply.github.com>
2023-03-22 03:06:28 +0000

    [Se removió país y edad de la clase actor]

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -70,20 +70,16 @@ namespace Pelicula
         //Propiedades
         private string Nombre;
         private Int16 Año;
-        private string País;
-        private Int16 Edad;
         //Constructores
-          public Actor (string Nombre, Int16 Año, string País, Int16 Edad)
+          public Actor (string Nombre, Int16 Año)
         {
             this.Nombre = Nombre; 
             this.Año = Año;
-            this.País = País;
-            this.Edad = Edad;
         }
         //Métodos 
         public void Imprime()
         {
-            Console.WriteLine($"{Nombre} {Año} {País} {Edad})");
+            Console.WriteLine($"{Nombre} {Año})");
         }
     }
 
@@ -98,12 +94,12 @@ namespace Pelicula
            Pelicula p2 = new Pelicula();
            p1.SetTitulo("Parasite");
            p1.SetAño(2019);
-           p1.AñadirActor(new Actor("Song Kang-ho", 1967,"Corea del Sur", 56));
+           p1.AñadirActor(new Actor("Song Kang-ho", 1967));
            p1.ImprimirActor();
            Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
            p2.SetTitulo("Everything Everywhere All At Once");
            p2.SetAño(2022);
-           p2.AñadirActor(new Actor ("Michelle Yeoh", 1962, "Malasia", 60));
+           p2.AñadirActor(new Actor ("Michelle Yeoh", 1962));
            p2.ImprimirActor();
            Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño());
         }
