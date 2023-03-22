7924bc80cbb9544efc23c67646160515210c7d13
Eduardo Galeana <124205317+L4l0G@users.noreply.github.com>
2023-03-22 02:43:44 +0000

    [Se muestran actores principales en Main]

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -55,7 +55,7 @@ namespace Pelicula
             Console.WriteLine($"{Titulo}({Año})");
         }
 
-        public void ImprimirActores()
+        public void ImprimirActor()
         {
             foreach (Actor protagonista in actores)
             {
@@ -98,9 +98,13 @@ namespace Pelicula
            Pelicula p2 = new Pelicula();
            p1.SetTitulo("Parasite");
            p1.SetAño(2019);
+           p1.AñadirActor(new Actor("Song Kang-ho", 1967,"Corea del Sur", 56));
+           p1.ImprimirActor();
            Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
            p2.SetTitulo("Everything Everywhere All At Once");
            p2.SetAño(2022);
+           p2.AñadirActor(new Actor ("Michelle Yeoh", 1962, "Malasia", 60));
+           p2.ImprimirActor();
            Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño());
         }
     }
