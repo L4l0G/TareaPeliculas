55a21f4e779a6141d51d7c554ef46e2bcc57454e
Eduardo Galeana <124205317+L4l0G@users.noreply.github.com>
2023-03-22 03:24:09 +0000

    [Se añadió La La Land como película]

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -96,10 +96,14 @@ namespace Pelicula
            p1.SetAño(2019);
            p1.AgregaActor(new Actor("Song Kang-ho", 1967));
            p1.ImprimeActores();
+           p1.AgregaActor(new Actor("Cho Yeo-jeong", 1981));
+           p1.ImprimeActores();
            Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
-           p2.SetTitulo("Everything Everywhere All At Once");
-           p2.SetAño(2022);
-           p2.AgregaActor(new Actor ("Michelle Yeoh", 1962));
+           p2.SetTitulo("La La Land");
+           p2.SetAño(2016);
+           p2.AgregaActor(new Actor ("Ryan Gosling", 1980));
+           p2.ImprimeActores();
+        p2.AgregaActor(new Actor ("Emma Stone", 1988));
            p2.ImprimeActores();
            Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño());
         }
